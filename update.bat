bcf9515301001970b835890241e0ee4e86c60d78
FAKE_Dream / <87318892+OcelotWalrus@users.noreply.github.com>
2023-01-08 19:53:00 +0100

    Added update scripts for both linux and windows

---
+++ update.bat
@@ -0,0 +1,3 @@
+ECHO started updating your local repo from github repo
+git pull origin main
+ECHO finishd updating your local repo from github repo
---
+++ update.sh
@@ -0,0 +1,3 @@
+echo started updating your local repo from github repo
+git pull origin main
+echo finishd updating your local repo from github repo
