194b30b628b26e20de36e601f6148589a7d61a4f
FAKE_Dream / <87318892+OcelotWalrus@users.noreply.github.com>
2023-02-09 19:26:51 +0100

    Updated bash scripts

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "Please enter the path of your endless sky plugin directory (https://pastebin.com/9h7cGgEW for help)"
+read path
+echo "Copying the locals plugin data into '$path'"
+cp -r plugin/ "$path"
+cd "$path"
+find . -depth -type d -name "plugin" -execdir mv {} "Cromha-expansion-plugin" \;
+sleep 1
+echo "Done"
