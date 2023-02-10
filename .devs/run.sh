fb20753ed0c129d6e2df38d410b6a94fc82cc94f
FAKE_Dream / <87318892+OcelotWalrus@users.noreply.github.com>
2023-02-10 14:30:26 +0100

    Updated bash scripts

---
+++ .devs/run.sh
@@ -0,0 +1,13 @@
+path=$(cat ../.settings)
+
+echo "Deleting the current plugin version in your endless sky plugins folder"
+rm -r "$path/Cromha-expansion-plugin"
+echo "Copying the locals plugin data into '$path'"
+cp -r ../plugin/ "$path"
+cd "$path"
+find . -depth -type d -name "plugin" -execdir mv {} "Cromha-expansion-plugin" \;
+echo "Running the endless sky game? (y/n)"
+read awnser
+if [ "$answer" == "y" ] ;then
+    endless-sky
+fi
--- update.sh
+++ update.sh
@@ -1,14 +1,12 @@
-default_path="cat .settings"
+path=$(cat .settings)
 
 # merging main into origin
 echo started updating your local repo from github repo
 git pull origin main
 echo finishd updating your local repo from github repo
 # running install.sh to reinstall the plugin
-echo "Please enter the path of your endless sky plugin directory (https://pastebin.com/9h7cGgEW for help):"
-read path
 echo "Deleting the current plugin data to overwrite with the new one"
-rm -r "$default_path/Cromha-expansion-plugin"
+rm -r "$path/Cromha-expansion-plugin"
 echo "Copying the locals plugin data into '$path'"
 cp -r plugin/ "$path"
 cd "$path"
