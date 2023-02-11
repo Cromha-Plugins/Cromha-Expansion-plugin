57bf85cd7bc5399fdb08388e28dbc1247e53bbc6
FAKE_Dream / <87318892+OcelotWalrus@users.noreply.github.com>
2023-02-11 20:56:08 +0100

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@ date=$(date)
 echo "Please enter the path of your endless sky plugin directory (https://pastebin.com/9h7cGgEW for help)"
 read path
 echo "Creating your .settings file to remember plugins path"
+rm .settings
 echo "$path" >> .settings
 echo "Copying the locals plugin data into '$path'"
 cp -r plugin/ "$path"
-sh scripts/logger.sh "Finished installing the Cromha-expansion-plugin"
+sh logger.sh "Finished installing the Cromha-expansion-plugin"
 cd "$path"
 find . -depth -type d -name "plugin" -execdir mv {} "Cromha-expansion-plugin" \;
 sleep 1
