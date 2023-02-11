f5a74cbb510613643cb707b79073e677c98ba2c7
FAKE_Dream / <87318892+OcelotWalrus@users.noreply.github.com>
2023-02-11 07:44:26 +0100

    Modified bash script run.sh

--- .devs/run.sh
+++ .devs/run.sh
@@ -5,6 +5,6 @@ echo "Deleting the current plugin version in your endless sky plugins folder"
 rm -r "$path/Cromha-expansion-plugin"
 echo "Copying the locals plugin data into '$path'"
 cp -r ../plugin/ "$path"
+sh ../logger.sh "Finished pushing current plugin version in the endless sky plugins folder"
 cd "$path"
 find . -depth -type d -name "plugin" -execdir mv {} "Cromha-expansion-plugin" \;
-sh ../logger.sh "Finished pushing current plugin version in the endless sky plugins folder"
