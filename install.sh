a7a7999335473bc032cbf7030521a880ed118abe
FAKE_Dream / <87318892+OcelotWalrus@users.noreply.github.com>
2023-02-12 08:02:40 +0100

    Updated install bash script

--- install.sh
+++ install.sh
@@ -2,11 +2,29 @@ date=$(date)
 
 echo "Please enter the path of your endless sky plugin directory (https://pastebin.com/9h7cGgEW for help)"
 read path
-echo -ne '#####                     (33%)\r'
+echo -ne '#####                    (10%)\r'
 sleep 1
-echo -ne '#############             (66%)\r'
+echo -ne '######                   (13%)\r'
 sleep 1
-echo -ne '#######################   (100%)\r'
+echo -ne '#######                  (18%)\r'
+sleep 1
+echo -ne '########                 (24%)\r'
+sleep 1
+echo -ne '###########              (37%)\r'
+sleep 1
+echo -ne '#############            (49%)\r'
+sleep 1
+echo -ne '##############           (54%)\r'
+sleep 1
+echo -ne '#################        (61%)\r'
+sleep 1
+echo -ne '##################       (66%)\r'
+sleep 1
+echo -ne '#######################  (76%)\r'
+sleep 1
+echo -ne '######################## (95%)\r'
+sleep 1
+echo -ne '#########################(100%)\r'
 echo -ne '\n'
 rm .settings
 echo "$path" >> .settings
