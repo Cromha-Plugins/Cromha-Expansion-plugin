e008a834729e7b81623a5d08afff11274601ba68
FAKE_Dream / <87318892+OcelotWalrus@users.noreply.github.com>
2023-02-19 19:33:18 +0100

    Did changes

--- install.py
+++ install.py
@@ -26,10 +26,10 @@ class Install:
 	sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
 
 	for i in range(toolbar_width):
-	    time.sleep(0.1) # do real work here
-	    # update the bar
-	    sys.stdout.write("#")
-	    sys.stdout.flush()
+		time.sleep(0.1) # do real work here
+		# update the bar
+		sys.stdout.write("#")
+		sys.stdout.flush()
 
 	sys.stdout.write("]\n") # this ends the progress bar
 	# Copy plugin to plugins directory
